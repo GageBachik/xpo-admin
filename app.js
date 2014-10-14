33c92bfa75b5087fd8e34d671fe72ec0f6c3ac94
gbachik <gbachik@gmail.com>
2014-10-13 21:50:40 -0600

    allow cors

--- app.js
+++ app.js
@@ -5,12 +5,21 @@ var indexController = require('./controllers/index.js');
 var apiController = require('./controllers/api.js');
 var port = process.env.PORT || 3000;
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'example.com');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
 var app = express();
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(session({secret: 'Ava123!'}))
+app.use(allowCrossDomain);
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://xpo-admin:Ava123!@linus.mongohq.com:10064/xpo-admin');
